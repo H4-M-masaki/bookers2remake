a91b186a98de54f1552bdde087e32f5e831f5f5d
EC2 Default User <ec2-user@ip-172-31-46-97.ec2.internal>
2022-02-01 12:38:53 +0000

    [add]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
   end
   
   
@@ -23,7 +24,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
   
   
--- app/models/book.rb
+++ app/models/book.rb
@@ -10,12 +10,12 @@ class Book < ApplicationRecord
   
   
   
-  def get_image
+  def get_image(size)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize: size).processed
   end
   
   
